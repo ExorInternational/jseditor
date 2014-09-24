91533dd7075f214fe3e3ab66595f4f96a2baee89
Roopak Sudhakar <roopaksudhakar@gmail.com>
2014-09-25 02:43:48 +0530

    Moved testApp project to outside the original source folder.

---
+++ TestApp/bin/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowState(Qt::WindowMaximized);
+
+    ui->mainToolBar->hide();
+
+    QMenu *fileMenu = ui->menuBar->addMenu("File");
+    fileMenu->addAction("Open", this, SLOT(onFileOpenClicked()));
+    fileMenu->addAction("Exit", qApp, SLOT(quit()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::onFileOpenClicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
+                                                     "/home",
+                                                     tr("Javascript Files (*.js)"));
+}
