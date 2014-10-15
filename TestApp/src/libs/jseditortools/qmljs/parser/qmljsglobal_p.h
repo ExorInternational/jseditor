a1c05baa32fa67cbe6b4201e56a3e98cfcd79c60
Roopak Sudhakar <roopaksudhakar@gmail.com>
2014-10-15 10:26:14 +0530

    Fixed an issue that was shown in Windows.

--- TestApp/src/libs/jseditortools/qmljs/parser/qmljsglobal_p.h
+++ TestApp/src/libs/jseditortools/qmljs/parser/qmljsglobal_p.h
@@ -29,31 +29,35 @@
 #ifndef QMLJSGLOBAL_P_H
 #define QMLJSGLOBAL_P_H
 
-#include <QtCore/qglobal.h>
+//#include <QtCore/qglobal.h>//#720 - ROOPAK START
 
-#ifdef QT_CREATOR
-#  define QT_QML_BEGIN_NAMESPACE
-#  define QT_QML_END_NAMESPACE
+//#ifdef QT_CREATOR
+//#  define QT_QML_BEGIN_NAMESPACE
+//#  define QT_QML_END_NAMESPACE
 
-#  ifdef QMLJS_BUILD_DIR
-#    define QML_PARSER_EXPORT Q_DECL_EXPORT
-#  elif QML_BUILD_STATIC_LIB
-#    define QML_PARSER_EXPORT
-#  else
-#    define QML_PARSER_EXPORT Q_DECL_IMPORT
-#  endif // QMLJS_BUILD_DIR
+//#  ifdef QMLJS_BUILD_DIR
+//#    define QML_PARSER_EXPORT Q_DECL_EXPORT
+//#  elif QML_BUILD_STATIC_LIB
+//#    define QML_PARSER_EXPORT
+//#  else
+//#    define QML_PARSER_EXPORT Q_DECL_IMPORT
+//#  endif // QMLJS_BUILD_DIR
 
-#else // !QT_CREATOR
+//#else // !QT_CREATOR//#720 - ROOPAK END
 #  define QT_QML_BEGIN_NAMESPACE QT_BEGIN_NAMESPACE
 #  define QT_QML_END_NAMESPACE QT_END_NAMESPACE
-#  if defined(QT_BUILD_QMLDEVTOOLS_LIB) || defined(QT_QMLDEVTOOLS_LIB)
-     // QmlDevTools is a static library
-#    define QML_PARSER_EXPORT
-#  elif defined(QT_BUILD_QML_LIB)
-#    define QML_PARSER_EXPORT Q_AUTOTEST_EXPORT
-#  else
-#    define QML_PARSER_EXPORT
-#  endif
-#endif // QT_CREATOR
+//#  if defined(QT_BUILD_QMLDEVTOOLS_LIB) || defined(QT_QMLDEVTOOLS_LIB)//#720 - ROOPAK START
+//     // QmlDevTools is a static library
+//#    define QML_PARSER_EXPORT
+//#  elif defined(QT_BUILD_QML_LIB)
+//#    define QML_PARSER_EXPORT Q_AUTOTEST_EXPORT
+//#  else
+//#    define QML_PARSER_EXPORT
+//#  endif
+//#endif // QT_CREATOR
+
+#include <qmljs/../jseditortools_global.h>
+
+#define QML_PARSER_EXPORT JSEDITORTOOLS_EXPORT//#720 - ROOPAK END
 
 #endif // QMLJSGLOBAL_P_H
